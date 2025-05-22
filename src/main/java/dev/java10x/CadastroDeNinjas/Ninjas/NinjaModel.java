72452dabd602488e5bf81329058fb18b7c16258e
luuh3k <lucas.digital18@gmail.com>
2025-05-21 23:15:48 -0300

    finalizando a model

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,17 +2,24 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //entity tranforma  uma  classe em uma entidade do banco de dados
 // JPA = JAVA PERSISTENCE API
 //GeneratedValue = a forma numerica de como ficaram salvos os id
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
 
     private String email;
@@ -24,36 +31,6 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id")// foreing key or chave estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
 
-    public NinjaModel(String nom, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
